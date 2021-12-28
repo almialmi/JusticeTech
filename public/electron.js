bbf511b52bfe0bffede4ae7cb8795312b0ec72fd
almialmi <almialmi61621@gmail.com>
2021-12-28 11:10:12 +0300

    add some change

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,8 @@
 const path = require('path');
-
-const { app, BrowserWindow } = require('electron');
+const { app,Menu,BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
+const isMac = process.platform === 'darwin'
+
 
 function createWindow() {
   // Create the browser window.
@@ -26,12 +27,56 @@ function createWindow() {
  /* if (isDev) {
     win.webContents.openDevTools({ mode: 'detach' });
   }*/
+
+
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.on('ready',()=>{
+  createWindow()
+
+  const template =[
+    {
+      label:'Demo',
+      submenu:[
+        { label: 'submenu1',
+          click:function(){
+            console.log("click submenu1")
+          }
+        },
+        { type: 'separator' },
+        { label: 'sebmenu2',
+          click:function(){
+            console.log("click submenu2")
+          }
+       }, 
+      ]
+    },
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectAll' },
+      ]
+    },
+  ]
+ const menu = Menu.buildFromTemplate(template);
+ Menu.setApplicationMenu(menu);
+
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -47,3 +92,5 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+
